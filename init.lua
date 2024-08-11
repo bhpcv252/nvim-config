954ebad3a794e6b039a203a6c90d9451d0b3bcf5
Sonu Singh <bhpcv252@gmail.com>
2024-08-12 03:33:10 +0530

    init

---
+++ init.lua
@@ -0,0 +1,25 @@
+local g = vim.g
+local o = vim.o
+
+o.number = true
+o.relativenumber = true
+o.autoindent = true
+o.tabstop = 4
+o.shiftwidth = 4
+
+o.clipboard = "unnamedplus"
+
+o.ignorecase = true
+o.smartcase = true
+
+o.history = 50
+
+o.splitright = true
+o.splitbelow = true
+
+o.mouse = "a"
+
+g.mapleader = " "
+g.maplocalleader = " "
+
+
