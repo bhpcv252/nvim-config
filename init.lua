b85f7664d6ed641b214c6b9626123fc6a948c2a7
Sonu Singh <bhpcv252@gmail.com>
2024-08-12 07:01:51 +0530

    lazy, colorscheme, finder added

--- init.lua
+++ init.lua
@@ -1,11 +1,16 @@
 local g = vim.g
 local o = vim.o
+local opt = vim.opt
+local cmd = vim.cmd
+
 
 o.number = true
 o.relativenumber = true
 o.autoindent = true
 o.tabstop = 4
 o.shiftwidth = 4
+
+o.termguicolors = true
 
 o.clipboard = "unnamedplus"
 
@@ -17,9 +22,72 @@ o.history = 50
 o.splitright = true
 o.splitbelow = true
 
-o.mouse = "a"
+opt.mouse = "a"
 
 g.mapleader = " "
 g.maplocalleader = " "
 
+
+
+-- Lazy Nvim package manager
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+
+-- Setup lazy.nvim
+require("lazy").setup({
+  spec = {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    },
+	{
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "go", "lua", "vim", "javascript", "html", "css", "json", "sql", "typescript" },
+          sync_install = false,
+          highlight = { enable = true },
+          indent = { enable = true },  
+        })
+    end
+ }
+  },
+  -- Configure any other settings here. See the documentation for more details.
+  -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
+  -- automatically check for plugin updates
+  checker = { enabled = true },
+})
+
+
+-- Catppuccin Colorscheme 
+require("catppuccin").setup()
+cmd.colorscheme "catppuccin"
+
+
+-- Telescope Fuzzy Finder
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
