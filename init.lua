e136ee2779c75dd02023d3c19a8d8eeb35ee87d7
Sonu Singh <bhpcv252@gmail.com>
2024-08-18 08:14:13 +0530

    neo tree set toggle, fold handler added

--- init.lua
+++ init.lua
@@ -125,7 +125,7 @@ require("neo-tree").setup({
 		},
 	}
 })
-vim.keymap.set('n', '<leader>oo', ':Neotree filesystem reveal left<CR>')
+vim.keymap.set('n', '<leader>oo', ':Neotree filesystem toggle left<CR>')
 
 
 -- Lualine
@@ -243,4 +243,34 @@ require("ibl").setup()
 
 
 -- Nvim UFO - for folding blocks
-require('ufo').setup()
+local foldHandler = function(virtText, lnum, endLnum, width, truncate)
+    local newVirtText = {}
+    local suffix = (' 󰁂 %d '):format(endLnum - lnum)
+    local sufWidth = vim.fn.strdisplaywidth(suffix)
+    local targetWidth = width - sufWidth
+    local curWidth = 0
+    for _, chunk in ipairs(virtText) do
+        local chunkText = chunk[1]
+        local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+        if targetWidth > curWidth + chunkWidth then
+            table.insert(newVirtText, chunk)
+        else
+            chunkText = truncate(chunkText, targetWidth - curWidth)
+            local hlGroup = chunk[2]
+            table.insert(newVirtText, {chunkText, hlGroup})
+            chunkWidth = vim.fn.strdisplaywidth(chunkText)
+            -- str width returned from truncate() may less than 2nd argument, need padding
+            if curWidth + chunkWidth < targetWidth then
+                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
+            end
+            break
+        end
+        curWidth = curWidth + chunkWidth
+    end
+    table.insert(newVirtText, {suffix, 'MoreMsg'})
+    return newVirtText
+end
+
+require('ufo').setup({
+	    fold_virt_text_handler = foldHandler
+})
