5084e89fcc8d436da5748481a9d532bf58e92ccf
Sonu Singh <bhpcv252@gmail.com>
2025-03-16 14:26:25 +0530

    added diffview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -164,10 +164,11 @@ return {
 	{
 		'stevearc/aerial.nvim',
 		opts = {},
-		-- Optional dependencies
 		dependencies = {
-			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons"
 		},
+	},
+	{
+		'sindrets/diffview.nvim',
 	}
 }
