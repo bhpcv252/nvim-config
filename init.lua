1f003cdefa79be1344c5aa27a9f25ede2061a95a
Sonu Singh <bhpcv252@gmail.com>
2025-03-27 01:31:19 +0530

    fixed bug

--- init.lua
+++ init.lua
@@ -1,4 +1,4 @@
-local g = vim.g
+local g = vim.gin
 local o = vim.o
 local opt = vim.opt
 
@@ -345,7 +345,6 @@ local null_ls = require("null-ls")
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
-	autostart = true,
 	sources = {
 		null_ls.builtins.formatting.black,
 		null_ls.builtins.formatting.stylua,
