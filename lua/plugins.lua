4a3720cab19bb1b71fb28d1f16e8d85fa5e26432
Sonu Singh <bhpcv252@gmail.com>
2025-03-31 22:00:54 +0530

    added markdown preview

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -170,5 +170,14 @@ return {
 	},
 	{
 		'sindrets/diffview.nvim',
-	}
+	},
+	{
+		"iamcco/markdown-preview.nvim",
+		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+		build = "cd app && yarn install",
+		init = function()
+			vim.g.mkdp_filetypes = { "markdown" }
+		end,
+		ft = { "markdown" },
+	},
 }
