d6f0acde0acd0d5d082003e3f2f06ae5e5f4c48f
Sonu Singh <bhpcv252@gmail.com>
2026-03-23 13:06:32 +0530

    added screenkey

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -191,4 +191,9 @@ return {
         end,
         ft = { "markdown" },
     },
+    {
+        "NStefan002/screenkey.nvim",
+        lazy = false,
+        version = "*", -- or branch = "main", to use the latest commit
+    }
 }
