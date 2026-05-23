3dd3e922079d00aa59dc5b8d5a4c1ee740df3588
Sonu Singh <bhpcv252@gmail.com>
2026-05-23 23:52:16 +0530

    Add Kafka tag in gopls

--- init.lua
+++ init.lua
@@ -248,7 +248,7 @@ lspconfig.gopls.setup({
         gopls = {
             completeUnimported = true,
             gofumpt = true,
-            buildFlags = { "-tags=integration,e2e" },
+            buildFlags = { "-tags=integration,e2e,kafka" },
         }
     }
 }) -- Go
