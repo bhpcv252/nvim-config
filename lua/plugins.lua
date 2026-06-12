04648e3ca9c87313ecba5cae43ccdab0b0fa25d6
Sonu Singh <sonu.s@octogle.com>
2026-06-12 17:30:53 +0530

    added claude code

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -195,5 +195,43 @@ return {
         "NStefan002/screenkey.nvim",
         lazy = false,
         version = "*", -- or branch = "main", to use the latest commit
+    },
+    {
+        "coder/claudecode.nvim",
+        config = true,
+        cmd = {
+            "ClaudeCode",
+            "ClaudeCodeFocus",
+            "ClaudeCodeSelectModel",
+            "ClaudeCodeAdd",
+            "ClaudeCodeSend",
+            "ClaudeCodeTreeAdd",
+            "ClaudeCodeStatus",
+            "ClaudeCodeStart",
+            "ClaudeCodeStop",
+            "ClaudeCodeOpen",
+            "ClaudeCodeClose",
+            "ClaudeCodeDiffAccept",
+            "ClaudeCodeDiffDeny",
+            "ClaudeCodeCloseAllDiffs",
+        },
+        keys = {
+            { "<leader>z",  nil,                              desc = "AI/Claude Code" },
+            { "<leader>zc", "<cmd>ClaudeCode<cr>",            desc = "Toggle Claude" },
+            { "<leader>zf", "<cmd>ClaudeCodeFocus<cr>",       desc = "Focus Claude" },
+            { "<leader>zr", "<cmd>ClaudeCode --resume<cr>",   desc = "Resume Claude" },
+            { "<leader>zC", "<cmd>ClaudeCode --continue<cr>", desc = "Continue Claude" },
+            { "<leader>zm", "<cmd>ClaudeCodeSelectModel<cr>", desc = "Select Claude model" },
+            { "<leader>zb", "<cmd>ClaudeCodeAdd %<cr>",       desc = "Add current buffer" },
+            { "<leader>zs", "<cmd>ClaudeCodeSend<cr>",        mode = "v",                  desc = "Send to Claude" },
+            {
+                "<leader>zs",
+                "<cmd>ClaudeCodeTreeAdd<cr>",
+                desc = "Add file",
+                ft = { "NvimTree", "neo-tree", "oil", "minifiles", "netrw", "snacks_picker_list" },
+            },
+            { "<leader>za", "<cmd>ClaudeCodeDiffAccept<cr>", desc = "Accept diff" },
+            { "<leader>zd", "<cmd>ClaudeCodeDiffDeny<cr>",   desc = "Deny diff" },
+        },
     }
 }
